1cd2cc1ec739e0bb3aa53fbcf46fa20530cfdb07
1220891583 <48829109+1220891583@users.noreply.github.com>
2021-06-22 10:20:55 +0800

    无修改

--- src/main/java/com/hhstudygroup/npustudy/domain/Question.java
+++ src/main/java/com/hhstudygroup/npustudy/domain/Question.java
@@ -68,4 +68,121 @@ public class Question implements Serializable {
 
     private Boolean deleted;
 
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getQuestionType() {
+        return questionType;
+    }
+
+    public void setQuestionType(Integer questionType) {
+        this.questionType = questionType;
+    }
+
+    public Integer getSubjectId() {
+        return subjectId;
+    }
+
+    public void setSubjectId(Integer subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public Integer getGradeLevel() {
+        return gradeLevel;
+    }
+
+    public void setGradeLevel(Integer gradeLevel) {
+        this.gradeLevel = gradeLevel;
+    }
+
+    public Integer getDifficult() {
+        return difficult;
+    }
+
+    public void setDifficult(Integer difficult) {
+        this.difficult = difficult;
+    }
+
+    public String getCorrect() {
+        return correct;
+    }
+
+    public void setCorrect(String correct) {
+        this.correct = correct;
+    }
+
+    public Integer getInfoTextContentId() {
+        return infoTextContentId;
+    }
+
+    public void setInfoTextContentId(Integer infoTextContentId) {
+        this.infoTextContentId = infoTextContentId;
+    }
+
+    public Integer getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(Integer createUser) {
+        this.createUser = createUser;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "id=" + id +
+                ", questionType=" + questionType +
+                ", subjectId=" + subjectId +
+                ", score=" + score +
+                ", gradeLevel=" + gradeLevel +
+                ", difficult=" + difficult +
+                ", correct='" + correct + '\'' +
+                ", infoTextContentId=" + infoTextContentId +
+                ", createUser=" + createUser +
+                ", status=" + status +
+                ", createTime=" + createTime +
+                ", deleted=" + deleted +
+                '}';
+    }
 }
